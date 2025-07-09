ee4d43a10fc1aecfc5e75b814cfe2c80be5dee5d
MazingBusiness <mazing.store21@gmail.com>
2025-07-09 11:52:21 +0530

    upload vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
 // --------------- WHEN BUILD OPEN THE BELOW CODE. AND CLOSE THE ABOVE CODE ----------------
 // import { defineConfig } from 'vite'
@@ -15,9 +15,9 @@ export default defineConfig({
 // });
 
 // --------------- WHEN UPLOAD THE BELOW CODE INTO GITHUB ----------------
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/demo_react_mazing_business",
-// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  base: "/demo_react_mazing_business",
+});
