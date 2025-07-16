6fd1dd78825d247ba73ca749da2b5edf60a43462
MazingBusiness <mazing.store21@gmail.com>
2025-07-16 13:46:12 +0530

    upload 404.html for git

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./styles/mainStyle.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/mazing_react_html/"> */}
+    {/* <BrowserRouter basename="/mazing_react_website/"> */}
     <BrowserRouter basename="/demo_react_mazing_business/">
     {/* <BrowserRouter> */}
       <AppRoutes />
