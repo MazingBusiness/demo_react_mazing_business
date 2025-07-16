313cda836671bfc51b9d453160e9080d2e35b8d3
MazingBusiness <mazing.store21@gmail.com>
2025-07-17 01:00:34 +0530

    correction code for other link open

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,6 @@ import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 import "./styles/mainStyle.css";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
@@ -23,11 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
+      <AppRoutes />
     </HashRouter>
   </React.StrictMode>
 );
