88142d662fb834605622db6af5a81af3b4f80981
MazingBusiness <mazing.store21@gmail.com>
2026-02-24 01:34:04 +0530

    Login from admin

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 import "./styles/mainStyle.css";
@@ -23,6 +24,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
       <Toaster position="top-right" />
-      <AppRoutes />
+      <HashRouter>
+        <AppRoutes />
+      </HashRouter>
   </React.StrictMode>
 );
