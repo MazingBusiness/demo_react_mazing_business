a3d03e34189d4fc4f492d67b7f092924a2722430
MazingBusiness <mazing.store21@gmail.com>
2026-02-24 02:03:12 +0530

    Login from admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import { Toaster } from "react-hot-toast";
+// import AppRoutes from "./routes/AppRoutes";
+// import "./styles/global.css";
+// import "./styles/mainStyle.css";
+
+
+// // For local server
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //   <React.StrictMode>
+// //     {/* <BrowserRouter basename="/mazing_react_website/"> */}
+// //     {/* <BrowserRouter basename="/demo_react_mazing_business/"> */}
+// //     <BrowserRouter>
+// //       <AppRoutes />
+// //     </BrowserRouter>
+// //   </React.StrictMode>
+// // );
+
+// // For Git
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//       <Toaster position="top-right" />
+//       <AppRoutes />
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,23 +35,21 @@ import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 import "./styles/mainStyle.css";
 
+const BASENAME = "/demo_react_mazing_business";
 
-// For local server
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     {/* <BrowserRouter basename="/mazing_react_website/"> */}
-//     {/* <BrowserRouter basename="/demo_react_mazing_business/"> */}
-//     <BrowserRouter>
-//       <AppRoutes />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
+// restore redirect from 404.html (if exists)
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  window.history.replaceState({}, "", redirect);
+}
 
-// For Git
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <Toaster position="top-right" />
+    <Toaster position="top-right" />
+    <BrowserRouter basename={BASENAME}>
       <AppRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );
