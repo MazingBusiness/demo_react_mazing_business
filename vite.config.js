a3264b06ae83c87f4b7695cb84f19b5e3455620f
MazingBusiness <mazing.store21@gmail.com>
2026-09-18 15:31:13 +0530

    Update build output with new nav buttons

--- vite.config.js
+++ vite.config.js
@@ -7,30 +7,11 @@
 
 // --------------- WHEN BUILD OPEN THE BELOW CODE. AND CLOSE THE ABOVE CODE ----------------
 // ------------------------------ Mazing Live -----------------------------
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-//   server: {
-//     proxy: {
-//       "/abm-api": {
-//         target: "https://abm.spurspaces.com",
-//         changeOrigin: true,
-//         secure: true,
-//         rewrite: (path) => path.replace(/^\/abm-api/, ""),
-//       },
-//     },
-//   },
-// });
-
-// ------------------------------ Mazing Development -----------------------------
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: "/mazing_react_website/",
+  base: "/",
   server: {
     proxy: {
       "/abm-api": {
@@ -42,6 +23,25 @@ export default defineConfig({
     },
   },
 });
+
+// ------------------------------ Mazing Development -----------------------------
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/mazing_react_website/",
+//   server: {
+//     proxy: {
+//       "/abm-api": {
+//         target: "https://abm.spurspaces.com",
+//         changeOrigin: true,
+//         secure: true,
+//         rewrite: (path) => path.replace(/^\/abm-api/, ""),
+//       },
+//     },
+//   },
+// });
 
 //--------------- WHEN UPLOAD THE BELOW CODE INTO GITHUB ----------------
 // import { defineConfig } from 'vite'
