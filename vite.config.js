a0c19a77774a9e9c39cda94c5a7fca5c4bee9ba9
MazingBusiness <mazing.store21@gmail.com>
2026-09-18 13:54:53 +0530

    Update vite.config.js base path for GitHub deployment

--- vite.config.js
+++ vite.config.js
@@ -25,28 +25,28 @@
 // });
 
 // ------------------------------ Mazing Development -----------------------------
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/mazing_react_website/",
-//   server: {
-//     proxy: {
-//       "/abm-api": {
-//         target: "https://abm.spurspaces.com",
-//         changeOrigin: true,
-//         secure: true,
-//         rewrite: (path) => path.replace(/^\/abm-api/, ""),
-//       },
-//     },
-//   },
-// });
-
-//--------------- WHEN UPLOAD THE BELOW CODE INTO GITHUB ----------------
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: "/demo_react_mazing_business",
+  base: "/mazing_react_website/",
+  server: {
+    proxy: {
+      "/abm-api": {
+        target: "https://abm.spurspaces.com",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/abm-api/, ""),
+      },
+    },
+  },
 });
+
+//--------------- WHEN UPLOAD THE BELOW CODE INTO GITHUB ----------------
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/demo_react_mazing_business",
+// });
